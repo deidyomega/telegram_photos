2e7eeedf4190c45a04d60a755dc5202eaedf7ca9
Matthew Harris <matthewharris24@gmail.com>
2021-10-10 18:00:00 -0700

    cleanup

--- src/main.py
+++ src/main.py
@@ -6,32 +6,37 @@ from telethon import TelegramClient, events
 import logging
 from webdav3.client import Client
 
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
 logger = logging.getLogger(__name__)
 load_dotenv()
 
-logger.info('Creating Client...')
-client = TelegramClient(
-    "SESSION",
-    os.environ['API_ID'],
-    os.environ['API_HASH']
-)
+logger.info("Creating Client...")
+client = TelegramClient("SESSION", os.environ["API_ID"], os.environ["API_HASH"])
+
 
 def webdavhandle(filename):
     options = {
-        'webdav_hostname': os.environ['WEBDAV_HOSTNAME'],
-        'webdav_login':    os.environ['WEBDAV_LOGIN'],
-        'webdav_password': os.environ['WEBDAV_PASSWORD']
+        "webdav_hostname": os.environ["WEBDAV_HOSTNAME"],
+        "webdav_login": os.environ["WEBDAV_LOGIN"],
+        "webdav_password": os.environ["WEBDAV_PASSWORD"],
     }
 
     client = Client(options)
     client.verify = False
-    logger.info('Sending to remote')
-    client.upload_sync(remote_path=f"Photos/Telegram/{filename}".replace(os.environ['FOLDER_PATH'] + "/",""), local_path=filename)
-    logger.info('Package delievered')
+    logger.info("Sending to remote")
+    client.upload_sync(
+        remote_path=f"Photos/Telegram/{filename}".replace(
+            os.environ["FOLDER_PATH"] + "/", ""
+        ),
+        local_path=filename,
+    )
+    logger.info("Package delievered")
+
 
 async def handle_file_event(event):
-    r = await event.message.download_media(os.environ['FOLDER_PATH'])
+    r = await event.message.download_media(os.environ["FOLDER_PATH"])
     webdavhandle(r)
     os.remove(r)
 
@@ -53,6 +58,9 @@ async def handler(event):
     else:
         logger.info("not doing anything")
 
-logger.info('Starting...')
-client.start(phone=os.environ['TELEGRAM_PHONE'], password=os.environ['TELEGRAM_PASSWORD'])
+
+logger.info("Starting...")
+client.start(
+    phone=os.environ["TELEGRAM_PHONE"], password=os.environ["TELEGRAM_PASSWORD"]
+)
 client.run_until_disconnected()
